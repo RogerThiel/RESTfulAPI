a23353a027e471ce6646685f2add7230394f0272
Roger Thiel <55242112+RogerThiel@users.noreply.github.com>
2024-03-26 16:11:55 +0100

    add Test Datenbankverbindung zu Elephant

--- db.js
+++ db.js
@@ -1,9 +1,22 @@
-const { Pool } = require('pg');
-const connectionString = 'postgres://wbprpxru:yiQx_C9_TLDrrVcjw3iiwG3ELUSgRhtq@balarama.db.elephantsql.com/wbprpxru';
-const pool = new Pool({ connectionString });
-
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback);
-  },
-};
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+  });
+  
+  const { Pool } = require('pg');
+  const connectionString = 'postgres://wbprpxru:yiQx_C9_TLDrrVcjw3iiwG3ELUSgRhtq@balarama.db.elephantsql.com/wbprpxru';
+  const pool = new Pool({ connectionString });
+  
+  pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+      console.error('Error executing query:', err.stack);
+    } else {
+      console.log('Query result:', res.rows[0]);
+    }
+    pool.end();
+  });
+  
+  module.exports = {
+    query: (text, params, callback) => {
+      return pool.query(text, params, callback);
+    },
+  };
