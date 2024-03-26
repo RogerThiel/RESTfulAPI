93579951d28d61ddc9b6c2c60ffca9567524fcb7
Roger Thiel <55242112+RogerThiel@users.noreply.github.com>
2024-03-26 13:15:28 +0100

    Updated ElephantSQL connection in db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const connectionString = 'Ihr-ElephantSQL-Verbindungsstring';
+const connectionString = 'postgres://wbprpxru:yiQx_C9_TLDrrVcjw3iiwG3ELUSgRhtq@balarama.db.elephantsql.com/wbprpxru';
 const pool = new Pool({ connectionString });
 
 module.exports = {
